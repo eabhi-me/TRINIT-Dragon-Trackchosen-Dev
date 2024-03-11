e636653cdd34f739b82f5f16f0c87fdd329bf992
eabhi-me <abhiydv423@gmail.com>
2024-03-12 00:06:35 +0530

    script js added and alert

---
+++ script.js
@@ -0,0 +1,7 @@
+// have to work on this
+
+for (let i = 0; i < 3; i++) {
+const say = document.querySelectorAll(".btn-me")[i].addEventListener("click", function(){
+    alert("Backend is under process.");
+});
+}
